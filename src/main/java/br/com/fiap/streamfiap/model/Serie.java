28c7c0ac784d04765ad6af046d1c5de65eec6e3a
Enzo <weeblyenzo@gmail.com>
2026-09-11 18:41:05 -0300

    fix: bug09 - Série cobrava preço padrão em vez do valor por temporada / Corrigido overload para override com @Override

--- src/main/java/br/com/fiap/streamfiap/model/Serie.java
+++ src/main/java/br/com/fiap/streamfiap/model/Serie.java
@@ -17,6 +17,7 @@ public class Serie extends Conteudo implements Promocionavel {
     }
 
     // preço da série: 4.90 por temporada
+    @Override
     public double calcularPrecoAluguel(double desconto) {
         return 4.90 * numeroTemporadas;
     }
